0a3f92d6515fdcd8054f219da960dcd774787134
Adam Tuma <adamtuma98@gmail.com>
2022-12-01 12:51:23 +0100

    intial commit

---
+++ 2022/day1_solution.py
@@ -0,0 +1,27 @@
+### PART 1 ###
+
+txt_file = open('day1_input.txt', 'r')
+data = [line.strip() for line in txt_file]
+#data = [i.replace("","0") for i in data]
+txt_file.close()
+
+calories = []
+i_kcal = 0
+
+for i in data:
+    if i == "":
+        calories.append(i_kcal)
+        i_kcal = 0 
+    else:
+       i_kcal += int(i)
+
+    
+kcal_max = max(calories)
+
+print(f"Answer for part one is: {kcal_max}")
+
+### PART 2 ###
+
+calories.sort(reverse = True)
+
+print(f"Answer for part two is: {sum(calories[0:3])}")
